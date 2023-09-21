ac3f3a8fecc82affea6d495df6c06c1e40b1557b
jabahum <jabajnan8@gmail.com>
2023-09-21 18:46:39 +0300

    fix: build

--- src/patient-chart/laboratory-order.component.tsx
+++ src/patient-chart/laboratory-order.component.tsx
@@ -1,72 +1,85 @@
 import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { EncounterList, EncounterListColumn, getObsFromEncounter } from "@ohri/openmrs-esm-ohri-commons-lib";
+import {
+  EncounterList,
+  EncounterListColumn,
+  getObsFromEncounter,
+} from "@ohri/openmrs-esm-ohri-commons-lib";
 import moment from "moment";
-import { moduleName, LABORATARORY_ENCOUNTER_TYPE, TEST_ORDER_ENCOUNTER_TYPE } from "../constants";
- 
+import {
+  moduleName,
+  LABORATARORY_ENCOUNTER_TYPE,
+  TEST_ORDER_ENCOUNTER_TYPE,
+} from "../constants";
 
 interface LaboratoryOrderOverviewProps {
   patientUuid: string;
 }
 
-const LaboratoryOrder: React.FC<LaboratoryOrderOverviewProps> = ({ patientUuid }) => {
-  const { t } = useTranslation();  
- 
+const LaboratoryOrder: React.FC<LaboratoryOrderOverviewProps> = ({
+  patientUuid,
+}) => {
+  const { t } = useTranslation();
+
   const columnsLab: EncounterListColumn[] = useMemo(
     () => [
       {
-        key: 'encounterDate',
-        header: t('encounterDate', 'Encounter Date'),
+        key: "encounterDate",
+        header: t("encounterDate", "Encounter Date"),
         getValue: (encounter) => {
-          return moment(encounter.encounterDatetime).format('DD-MMM-YYYY');
+          return moment(encounter.encounterDatetime).format("DD-MMM-YYYY");
         },
-      },  
+      },
       {
-        key: 'testOrders',
-        header: t('deliveryType', 'Test Order(s)'),
+        key: "testOrders",
+        header: t("deliveryType", "Test Order(s)"),
         getValue: (encounter) => {
           return getObsFromEncounter(encounter, TEST_ORDER_ENCOUNTER_TYPE);
         },
       },
 
       {
-        key: 'actions',
-        header: t('actions', 'Actions'),
+        key: "actions",
+        header: t("actions", "Actions"),
         getValue: (encounter) => {
           const baseActions = [
             {
-              form: { name: 'HMIS LAB 001:General Laboratory Test Request Form'},
+              form: {
+                name: "HMIS LAB 001:General Laboratory Test Request Form",
+              },
               encounterUuid: encounter.uuid,
-              intent: '*',
-              label: 'View Details',
-              mode: 'view',
+              intent: "*",
+              label: "View Details",
+              mode: "view",
             },
             {
-              form: { name: 'HMIS LAB 001:General Laboratory Test Request Form' },
+              form: {
+                name: "HMIS LAB 001:General Laboratory Test Request Form",
+              },
               encounterUuid: encounter.uuid,
-              intent: '*',
-              label: 'Edit Form',
-              mode: 'edit',
+              intent: "*",
+              label: "Edit Form",
+              mode: "edit",
             },
           ];
           return baseActions;
         },
       },
     ],
-    [t],
+    [t]
   );
-  const headerTitle = t('Laboratory');
+  const headerTitle = t("Laboratory");
 
   return (
     <EncounterList
       patientUuid={patientUuid}
       encounterType={LABORATARORY_ENCOUNTER_TYPE}
-      formList={[{ name: 'HMIS LAB 001:General Laboratory Test Request Form' }]}
+      formList={[{ name: "HMIS LAB 001:General Laboratory Test Request Form" }]}
       columns={columnsLab}
       description={headerTitle}
       headerTitle={headerTitle}
       launchOptions={{
-        displayText: 'Add',
+        displayText: "Add",
         moduleName: moduleName,
       }}
     />
