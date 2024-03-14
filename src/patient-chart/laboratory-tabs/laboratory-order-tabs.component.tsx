a164cb41ec62eaf03de18fbe345d5da547fbc9cc
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2024-03-14 14:37:16 +0300

    (feat) Change investigative results  tables active tests and past tests view to tab views (#62)
    * refactor investigative results
    
    * move styles to .scss
    
    * move styles to .scss

--- src/patient-chart/laboratory-tabs/laboratory-order-tabs.component.tsx
+++ src/patient-chart/laboratory-tabs/laboratory-order-tabs.component.tsx
@@ -3,7 +3,8 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@carbon/react";
 import styles from "./laboratory-order-tabs.scss";
 import { useTranslation } from "react-i18next";
 import PatientLaboratoryReferalResults from "../laboratory-order-referals/laboratory-order-referals.component";
-import PatientLaboratoryResults from "../patient-laboratory-results.component";
+import LaboratoryPastTestOrderResults from "../laboratory-past-test/laboratory-past-test-order-results.component";
+import LaboratoryActiveTestOrderResults from "../laboratory-active-test-order/laboratory-active-test-order-results.component";
 
 interface LaboratoryResultsTabsProps {
   patientUuid: string;
@@ -25,21 +26,31 @@ const LaboratoryResultsTabs: React.FC<LaboratoryResultsTabsProps> = ({
           className={styles.tabs}
         >
           <TabList
-            style={{ paddingLeft: "1rem" }}
+            className={styles.tabList}
             aria-label="laboratory results tabs"
             contained
           >
-            <Tab style={{ width: "150px" }}>
-              {t("pending", "Routine Tests")}
+            <Tab className={styles.tabWidth}>
+              {t("pending", "Active Tests")}
             </Tab>
-            <Tab style={{ width: "150px" }}>{t("referals", "Referrals")}</Tab>
+            <Tab className={styles.tabWidth}>{t("pending", "Past Tests")}</Tab>
+            <Tab className={styles.tabWidth}>
+              {t("referals", "Referred Tests")}
+            </Tab>
           </TabList>
           <TabPanels>
-            <TabPanel style={{ padding: 0 }}>
-              <PatientLaboratoryResults patientUuid={patientUuid} />
+            <TabPanel>
+              <div className={styles.panelContainer}>
+                <LaboratoryActiveTestOrderResults patientUuid={patientUuid} />
+              </div>
             </TabPanel>
-            <TabPanel style={{ padding: 0 }}>
-              <div style={{ margin: "10px" }}>
+            <TabPanel>
+              <div className={styles.panelContainer}>
+                <LaboratoryPastTestOrderResults patientUuid={patientUuid} />
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className={styles.panelContainer}>
                 <PatientLaboratoryReferalResults patientUuid={patientUuid} />
               </div>
             </TabPanel>
