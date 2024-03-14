566010ec07b8b659e1e105af8f73a07fee46d96b
jabahum <jabajnan8@gmail.com>
2024-03-14 11:38:46 +0300

    refactor investigative results

--- src/patient-chart/laboratory-tabs/laboratory-order-tabs.component.tsx
+++ src/patient-chart/laboratory-tabs/laboratory-order-tabs.component.tsx
@@ -3,7 +3,8 @@ import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@carbon/react";
 import styles from "./laboratory-order-tabs.scss";
 import { useTranslation } from "react-i18next";
 import PatientLaboratoryReferalResults from "../laboratory-order-referals/laboratory-order-referals.component";
-import PatientLaboratoryResults from "../patient-laboratory-results.component";
+import LaboratoryPastTestOrderResults from "../laboratory-past-test/laboratory-past-test-order-results.component";
+import LaboratoryActiveTestOrderResults from "../laboratory-active-test-order/laboratory-active-test-order-results.component";
 
 interface LaboratoryResultsTabsProps {
   patientUuid: string;
@@ -29,14 +30,22 @@ const LaboratoryResultsTabs: React.FC<LaboratoryResultsTabsProps> = ({
             aria-label="laboratory results tabs"
             contained
           >
+            <Tab style={{ width: "150px" }}>{t("pending", "Active Tests")}</Tab>
+            <Tab style={{ width: "150px" }}>{t("pending", "Past Tests")}</Tab>
             <Tab style={{ width: "150px" }}>
-              {t("pending", "Routine Tests")}
+              {t("referals", "Referred Tests")}
             </Tab>
-            <Tab style={{ width: "150px" }}>{t("referals", "Referrals")}</Tab>
           </TabList>
           <TabPanels>
             <TabPanel style={{ padding: 0 }}>
-              <PatientLaboratoryResults patientUuid={patientUuid} />
+              <div style={{ margin: "10px" }}>
+                <LaboratoryActiveTestOrderResults patientUuid={patientUuid} />
+              </div>
+            </TabPanel>
+            <TabPanel style={{ padding: 0 }}>
+              <div style={{ margin: "10px" }}>
+                <LaboratoryPastTestOrderResults patientUuid={patientUuid} />
+              </div>{" "}
             </TabPanel>
             <TabPanel style={{ padding: 0 }}>
               <div style={{ margin: "10px" }}>
