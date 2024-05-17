6884201505e7ce53ea28cb9679ef389f9b862652
Bhargav kodali <115476530+kb019@users.noreply.github.com>
2024-05-18 02:04:45 +0530

    (fix) O3-3201 Fix 400 error (#68)

--- src/laboratory-resource.ts
+++ src/laboratory-resource.ts
@@ -50,7 +50,7 @@ export function setFulfillerStatus(
   status: FulfillerStatus,
   abortController: AbortController
 ) {
-  return openmrsFetch(`{$restBaseUrl}/order/${orderId}/fulfillerdetails/`, {
+  return openmrsFetch(`${restBaseUrl}/order/${orderId}/fulfillerdetails/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
