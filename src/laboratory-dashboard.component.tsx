e0845f786fd69c197bc1621661c5cfe160a968f6
Twiineenock <twiineenockfox@gmail.com>
2024-08-11 01:22:33 +0300

    use styleguide pageHeader

--- src/laboratory-dashboard.component.tsx
+++ src/laboratory-dashboard.component.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { LaboratoryHeader } from "./header/laboratory-header.component";
+import { useTranslation } from "react-i18next";
+import { PageHeader, LaboratoryPictogram } from "@openmrs/esm-framework";
 import Overlay from "./components/overlay/overlay.component";
 import LaboratoryOrdersTabs from "./lab-tabs/laboratory-tabs.component";
 import LaboratorySummaryTiles from "./lab-tiles/laboratory-summary-tiles.component";
+import styles from "./laboratory-dashboard.scss";
 
 const LaboratoryDashboard: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={`omrs-main-content`}>
-      <LaboratoryHeader />
+      <PageHeader
+        illustration={<LaboratoryPictogram />}
+        title={t("laboratory", "Laboratory")}
+        className={styles.pageHeader}
+      />
       <LaboratorySummaryTiles />
       <LaboratoryOrdersTabs />
       <Overlay />
