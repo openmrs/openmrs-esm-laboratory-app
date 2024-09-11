859c60242d3a72e9122da463a2064356ec771700
Amos Machora <amosmachora13@gmail.com>
2024-09-11 11:58:03 +0300

    refactor: simple UI layout improvements

--- src/components/orders-table/orders-date-range-picker.tsx
+++ src/components/orders-table/orders-date-range-picker.tsx
@@ -12,26 +12,20 @@ export const OrdersDateRangePicker = ({
 }) => {
   const currentDate = new Date();
   const { t } = useTranslation();
+
   return (
-    <DatePicker
-      datePickerType="range"
-      className={styles.dateRangePicker}
-      onClose={onChange}
-      maxDate={currentDate.toISOString()}
-      value={currentValues}
-    >
-      <DatePickerInput
-        id="date-picker-input-id-start"
-        placeholder="mm/dd/yyyy"
-        labelText={t('startDate', 'Start date')}
-        size="md"
-      />
-      <DatePickerInput
-        id="date-picker-input-id-finish"
-        placeholder="mm/dd/yyyy"
-        labelText={t('endDate', 'End date')}
-        size="md"
-      />
-    </DatePicker>
+    <div className={styles.datePickerWrapper}>
+      <p>{t('dateRange', 'date range')}:</p>
+      <DatePicker
+        datePickerType="range"
+        className={styles.dateRangePicker}
+        onClose={onChange}
+        maxDate={currentDate.toISOString()}
+        value={currentValues}
+      >
+        <DatePickerInput id="date-picker-input-id-start" placeholder="mm/dd/yyyy" size="md" />
+        <DatePickerInput id="date-picker-input-id-finish" placeholder="mm/dd/yyyy" size="md" />
+      </DatePicker>
+    </div>
   );
 };
