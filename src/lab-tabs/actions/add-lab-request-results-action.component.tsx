f64e99ef1c2c63f3706b59022c5e6683956d063f
Amos Machora <amosmachora13@gmail.com>
2025-02-03 12:29:47 +0300

    fix: pass mutate lab orders funtion to lab results form for accurate swr mutation

--- src/lab-tabs/actions/add-lab-request-results-action.component.tsx
+++ src/lab-tabs/actions/add-lab-request-results-action.component.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Button } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
-import { launchWorkspace } from '@openmrs/esm-framework';
+import { type Config, launchWorkspace, restBaseUrl, useConfig } from '@openmrs/esm-framework';
 import { type Order } from '@openmrs/esm-patient-common-lib';
 import styles from './actions.scss';
+import { mutate } from 'swr';
 
 interface AddLabRequestResultsActionProps {
   order: Order;
 }
 const AddLabRequestResultsAction: React.FC<AddLabRequestResultsActionProps> = ({ order }) => {
   const { t } = useTranslation();
+  const { laboratoryOrderTypeUuid } = useConfig<Config>();
 
   return (
     <Button
@@ -17,7 +19,17 @@ const AddLabRequestResultsAction: React.FC<AddLabRequestResultsActionProps> = ({
       kind="primary"
       size="sm"
       key={`${order.uuid}`}
-      onClick={() => launchWorkspace('test-results-form-workspace', { order })}
+      onClick={() => {
+        launchWorkspace('test-results-form-workspace', {
+          order,
+          mutateLabOrders: () => {
+            mutate(
+              (key) =>
+                typeof key === 'string' && key.startsWith(`${restBaseUrl}/order?orderTypes=${laboratoryOrderTypeUuid}`),
+            );
+          },
+        });
+      }}
     >
       {t('addLabResult', 'Add lab results')}
     </Button>
