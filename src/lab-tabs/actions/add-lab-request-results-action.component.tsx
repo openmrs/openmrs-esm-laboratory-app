ed949bc608a601c80c85d0eec45d766f914d92ef
Amos Machora <amosmachora13@gmail.com>
2025-02-04 09:48:39 +0300

    refactor: use callback on button click handlers to maintain a strict function reference across rerenders

--- src/lab-tabs/actions/add-lab-request-results-action.component.tsx
+++ src/lab-tabs/actions/add-lab-request-results-action.component.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from '@carbon/react';
+import { mutate } from 'swr';
 import { useTranslation } from 'react-i18next';
-import { type Config, launchWorkspace, restBaseUrl, useConfig } from '@openmrs/esm-framework';
+import { type Config } from '../../config-schema';
+import { launchWorkspace, restBaseUrl, useConfig } from '@openmrs/esm-framework';
 import { type Order } from '@openmrs/esm-patient-common-lib';
 import styles from './actions.scss';
-import { mutate } from 'swr';
 
 interface AddLabRequestResultsActionProps {
   order: Order;
@@ -13,24 +14,21 @@ const AddLabRequestResultsAction: React.FC<AddLabRequestResultsActionProps> = ({
   const { t } = useTranslation();
   const { laboratoryOrderTypeUuid } = useConfig<Config>();
 
+  const mutateLabOrders = useCallback(() => {
+    mutate(
+      (key) => typeof key === 'string' && key.startsWith(`${restBaseUrl}/order?orderTypes=${laboratoryOrderTypeUuid}`),
+    );
+  }, [laboratoryOrderTypeUuid]);
+
+  const launchTestResultsWorkspace = useCallback(() => {
+    launchWorkspace('test-results-form-workspace', {
+      order,
+      mutateLabOrders,
+    });
+  }, [mutateLabOrders, order]);
+
   return (
-    <Button
-      className={styles.actionButton}
-      kind="primary"
-      size="sm"
-      key={`${order.uuid}`}
-      onClick={() => {
-        launchWorkspace('test-results-form-workspace', {
-          order,
-          mutateLabOrders: () => {
-            mutate(
-              (key) =>
-                typeof key === 'string' && key.startsWith(`${restBaseUrl}/order?orderTypes=${laboratoryOrderTypeUuid}`),
-            );
-          },
-        });
-      }}
-    >
+    <Button className={styles.actionButton} kind="primary" size="sm" onClick={launchTestResultsWorkspace}>
       {t('addLabResult', 'Add lab results')}
     </Button>
   );
