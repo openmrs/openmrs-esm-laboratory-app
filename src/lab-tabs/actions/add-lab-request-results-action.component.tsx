44578b502e61db1e566e0c75a740d95cb856bd1a
Dennis Kigen <kigen.work@gmail.com>
2025-02-17 23:23:24 +0300

    Fixup

--- src/lab-tabs/actions/add-lab-request-results-action.component.tsx
+++ src/lab-tabs/actions/add-lab-request-results-action.component.tsx
@@ -1,34 +1,35 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Button } from '@carbon/react';
 import { mutate } from 'swr';
 import { useTranslation } from 'react-i18next';
-import { type Config } from '../../config-schema';
 import { launchWorkspace, restBaseUrl, useConfig } from '@openmrs/esm-framework';
 import { type Order } from '@openmrs/esm-patient-common-lib';
+import { type Config } from '../../config-schema';
 import styles from './actions.scss';
 
 interface AddLabRequestResultsActionProps {
   order: Order;
 }
+
 const AddLabRequestResultsAction: React.FC<AddLabRequestResultsActionProps> = ({ order }) => {
   const { t } = useTranslation();
   const { laboratoryOrderTypeUuid } = useConfig<Config>();
 
-  const mutateLabOrders = useCallback(() => {
+  const invalidateLabOrders = () => {
     mutate(
       (key) => typeof key === 'string' && key.startsWith(`${restBaseUrl}/order?orderTypes=${laboratoryOrderTypeUuid}`),
     );
-  }, [laboratoryOrderTypeUuid]);
+  };
 
-  const launchTestResultsWorkspace = useCallback(() => {
+  const launchTestResultsWorkspace = () => {
     launchWorkspace('test-results-form-workspace', {
       order,
-      mutateLabOrders,
+      invalidateLabOrders,
     });
-  }, [mutateLabOrders, order]);
+  };
 
   return (
-    <Button className={styles.actionButton} kind="primary" size="sm" onClick={launchTestResultsWorkspace}>
+    <Button className={styles.actionButton} kind="primary" onClick={launchTestResultsWorkspace} size="sm">
       {t('addLabResult', 'Add lab results')}
     </Button>
   );
