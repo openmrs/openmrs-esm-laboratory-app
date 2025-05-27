f4ba7f37e5c360176f933c515ee34736854ae73b
Dennis Kigen <kigen.work@gmail.com>
2025-05-27 11:59:27 +0300

    (chore) O3-4745: Fix failing dependency auto-update CI workflow
    This PR effects a host of changes to get this app in alignment with Core following [this change](https://github.com/openmrs/openmrs-esm-core/pull/1346). Presently, both [tests and the build script](https://github.com/openmrs/openmrs-esm-template-app/actions/workflows/update-openmrs-deps.yml) are failing. These changes include:
    
    - Bumping the `openmrs` and `@openmrs/esm-framework` libraries to the latest versions to get us in alignment with Core.
    - Bumping `@carbon/react`, `webpack` and `webpack-cli` to align with the versions in Core.
    - Amending the `transform` and `transformIgnorePatterns` configuration properties in the Jest config to ensure that ES modules in the `@openmrs` package (and other necessary dependencies) are properly transformed before being executed by Jest.
    - Fixing some Carbon type errors.

--- src/lab-tabs/laboratory-tabs.component.tsx
+++ src/lab-tabs/laboratory-tabs.component.tsx
@@ -15,56 +15,54 @@ const LaboratoryOrdersTabs: React.FC = () => {
   return (
     <main className={`omrs-main-content`}>
       <section className={styles.orderTabsContainer}>
-        <Tabs
-          selectedIndex={selectedTab}
-          onChange={({ selectedIndex }) => setSelectedTab(selectedIndex)}
-          className={styles.tabs}
-        >
-          <TabList style={{ paddingLeft: '1rem' }} aria-label="Laboratory tabs" contained>
-            {tabExtensions
-              .filter((extension) => Object.keys(extension.meta).length > 0)
-              .map((extension, index) => {
-                const { name, title } = extension.meta;
+        <div className={styles.tabs}>
+          <Tabs selectedIndex={selectedTab} onChange={({ selectedIndex }) => setSelectedTab(selectedIndex)}>
+            <TabList style={{ paddingLeft: '1rem' }} aria-label="Laboratory tabs" contained>
+              {tabExtensions
+                .filter((extension) => Object.keys(extension.meta).length > 0)
+                .map((extension, index) => {
+                  const { name, title } = extension.meta;
 
-                if (name && title) {
+                  if (name && title) {
+                    return (
+                      <Tab key={index} className={styles.tab} id={`${title || index}-tab`}>
+                        {t(title, {
+                          ns: extension.moduleName,
+                          defaultValue: title,
+                        })}
+                      </Tab>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+            </TabList>
+            <TabPanels>
+              {tabExtensions
+                .filter((extension) => Object.keys(extension.meta).length > 0)
+                .map((extension, index) => {
                   return (
-                    <Tab key={index} className={styles.tab} id={`${title || index}-tab`}>
-                      {t(title, {
-                        ns: extension.moduleName,
-                        defaultValue: title,
-                      })}
-                    </Tab>
+                    <TabPanel key={`${extension.meta.title}-tab-${index}`}>
+                      <ComponentContext.Provider
+                        key={extension.id}
+                        value={{
+                          moduleName: extension.moduleName,
+                          featureName: 'laboratory',
+                          extension: {
+                            extensionId: extension.id,
+                            extensionSlotName: labPanelSlot,
+                            extensionSlotModuleName: extension.moduleName,
+                          },
+                        }}
+                      >
+                        <Extension />
+                      </ComponentContext.Provider>
+                    </TabPanel>
                   );
-                } else {
-                  return null;
-                }
-              })}
-          </TabList>
-          <TabPanels>
-            {tabExtensions
-              .filter((extension) => Object.keys(extension.meta).length > 0)
-              .map((extension, index) => {
-                return (
-                  <TabPanel key={`${extension.meta.title}-tab-${index}`}>
-                    <ComponentContext.Provider
-                      key={extension.id}
-                      value={{
-                        moduleName: extension.moduleName,
-                        featureName: 'laboratory',
-                        extension: {
-                          extensionId: extension.id,
-                          extensionSlotName: labPanelSlot,
-                          extensionSlotModuleName: extension.moduleName,
-                        },
-                      }}
-                    >
-                      <Extension />
-                    </ComponentContext.Provider>
-                  </TabPanel>
-                );
-              })}
-          </TabPanels>
-        </Tabs>
+                })}
+            </TabPanels>
+          </Tabs>
+        </div>
       </section>
     </main>
   );
