0ece214b7fc06732802cb8ab1cae0698f6a9405c
praneeth <praneeth7077@gmail.com>
2025-06-20 15:04:27 +0530

    (feat) O3-4710: Redesign ListOrderDetails component  (#174)
    * (refactor) Simplify ListOrderDetails component by extracting OrderDetailRow for better readability
    
    * fixup
    
    * (refactor) Update spacing and remove unused styles in list-order-details.scss for improved layout consistency
    
    * fix: Correct ordererName and orderInStruction keys for consistency in translations
    
    * (chore) Update OpenMRS dependencies (#172)
    
    * (chore) Update OpenMRS dependencies (#173)
    
    * fix: Update OrderDetailRow for urgency status and improve label consistency
    
    * fixup
    
    * fix: Increase padding for priorityPill and statusPill for better visual consistency
    
    * fix: Update OrderDetailRow to display status pill for order status
    
    * fix
    
    * fix: Update priorityPill background color for routine urgency status
    
    * Remove extra space in orderInstructions string
    
    * Refactor urgency and status handling to use uppercase values for consistency
    
    * Improve urgency and status display formatting in ListOrderDetails component
    
    * Add missing translation for 'Order not picked' in en.json
    
    * Fix capitalization in 'orderedBy' translation and update ListOrderDetails component for cleaner rendering
    
    * imports in ListOrderDetails component for improved organization
    
    * fiixup
    
    ---------
    
    Co-authored-by: openmrs-bot <infrastructure@openmrs.org>

--- src/components/orders-table/list-order-details.component.tsx
+++ src/components/orders-table/list-order-details.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import type { ReactNode } from 'react';
 import {
   Accordion,
   AccordionItem,
@@ -7,119 +8,111 @@ import {
   StructuredListCell,
   StructuredListRow,
   StructuredListWrapper,
-  Tag,
 } from '@carbon/react';
 import { capitalize } from 'lodash-es';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import { type ListOrdersDetailsProps } from '../../types';
 import styles from './list-order-details.scss';
 
-const ListOrderDetails: React.FC<ListOrdersDetailsProps> = (props) => {
+type OrderDetailsRowProps = {
+  label: ReactNode;
+  value: ReactNode;
+};
+
+const OrderDetailRow = ({ label, value }: OrderDetailsRowProps) => {
+  return (
+    <StructuredListRow className={styles.orderDetailsRow}>
+      <StructuredListCell className={styles.orderDetailsCell}>
+        <span className={styles.orderDetailsTextBold}>{label}</span>
+      </StructuredListCell>
+      <StructuredListCell className={styles.orderDetailsCell}>
+        <span className={styles.orderDetailsText}>{value}</span>
+      </StructuredListCell>
+    </StructuredListRow>
+  );
+};
+const ListOrderDetails: React.FC<ListOrdersDetailsProps> = ({ groupedOrders }) => {
   const { t } = useTranslation();
-  const orders = props.groupedOrders?.orders;
+  const orders = groupedOrders?.orders ?? [];
 
   return (
     <div>
-      {orders &&
-        orders.map((row) => (
-          <div className={styles.orderDetailsContainer}>
-            <div className={styles.orderUrgency}></div>
-            <div className={styles.orderHeader}>
-              <span className={styles.orderNumber}>
-                {t('orderNumbers', 'Order number:')} {row.orderNumber}
-              </span>
-              <span className={styles.orderDate}>
-                {t('orderDate', 'Order Date:')} {row.dateActivated}
-              </span>
-            </div>
-            <div className={styles.orderStatus}>
-              {t('orderStatus', 'Status:')}
-              <Tag size="lg" type={row.fulfillerStatus ? 'green' : 'red'}>
-                {row.fulfillerStatus || t('orderNotPicked', 'Order not picked')}
-              </Tag>
-            </div>
-            <div>
-              <div className={styles.orderUrgency}>
-                <span className={styles.urgencyStatus}>
-                  {t('urgencyStatus', 'Urgency: ')} {capitalize(row.urgency)}
-                </span>
-              </div>
-              <StructuredListWrapper>
-                <StructuredListBody>
-                  <StructuredListRow>
-                    <StructuredListCell>{t('testOrdered', 'Test ordered: ')}</StructuredListCell>
-                    <StructuredListCell>{capitalize(row?.display)}</StructuredListCell>
-                    <br />
-                    <StructuredListCell>
-                      <div className={styles.instructionLabelContainer}>
-                        <span className={styles.instructionLabel}>{t('orderInStruction', 'Instructions: ')}</span>
-                        <span className={styles.instructions}>
-                          {row.instructions ?? (
-                            <Tag size="lg" type="red">
-                              {t('NoInstructionLeft', 'No instructions are provided.')}
-                            </Tag>
-                          )}
-                        </span>
-                      </div>
-                    </StructuredListCell>
-                  </StructuredListRow>
-                </StructuredListBody>
-              </StructuredListWrapper>
-              {row.fulfillerStatus === 'COMPLETED' && (
-                <Accordion>
-                  <AccordionItem
-                    title={<span className={styles.accordionTitle}>{t('viewTestResults', 'View test results')}</span>}
+      {orders.map((row) => (
+        <div key={row.orderNumber} className={styles.orderDetailsContainer}>
+          <StructuredListWrapper className={styles.orderDetailsWrapper}>
+            <StructuredListBody>
+              <OrderDetailRow
+                label={t('urgencyStatus', 'Urgency:')}
+                value={
+                  <div className={styles.priorityPill} data-urgency={row.urgency?.replace('_', ' ')}>
+                    {capitalize(row.urgency?.replace(/_/g, ' '))}
+                  </div>
+                }
+              />
+              <OrderDetailRow label={t('testOrdered', 'Test ordered:')} value={capitalize(row.display)} />
+              <OrderDetailRow
+                label={t('orderStatus', 'Status:')}
+                value={
+                  <div
+                    className={styles.statusPill}
+                    data-status={(row.fulfillerStatus ?? 'Order not picked').replace('_', ' ')}
                   >
-                    <div className={styles.viewResults}>
-                      <ExtensionSlot
-                        className={styles.labResultSlot}
-                        state={{ order: row }}
-                        name="completed-lab-order-results-slot"
-                      />
-                    </div>
-                  </AccordionItem>
-                </Accordion>
-              )}
-              {row.fulfillerStatus === 'DECLINED' && (
-                <StructuredListRow>
-                  <StructuredListCell> {t('reasonForDecline', 'Reason for decline:')}</StructuredListCell>
-                  <StructuredListCell>{row.fulfillerComment}</StructuredListCell>
-                </StructuredListRow>
-              )}
-              <StructuredListRow className={styles.nameOrderRow}>
-                <StructuredListCell>
-                  <span className={styles.nameOrder}>
-                    {t('ordererName', 'Orderer Name: ')} {capitalize(row.orderer?.display)}
-                  </span>
-                </StructuredListCell>
-              </StructuredListRow>
+                    {capitalize(row.fulfillerStatus?.replace('_', '')) || t('orderNotPicked', 'Order not picked')}
+                  </div>
+                }
+              />
+              <OrderDetailRow label={t('orderNumbers', 'Order number:')} value={capitalize(row.orderNumber)} />
+              <OrderDetailRow label={t('orderDate', 'Order Date:')} value={row.dateActivated} />
+              <OrderDetailRow label={t('orderedBy', 'Ordered By:')} value={capitalize(row.orderer?.display)} />
+              <OrderDetailRow
+                label={t('orderInstructions', 'Instructions:')}
+                value={row.instructions ?? t('NoInstructionLeft', 'No instructions are provided.')}
+              />
 
-              <div className={styles.buttonSection}>
-                {/* @ts-ignore */}
-                {row.fulfillerStatus === 'New' || row.fulfillerStatus === 'RECEIVED' || row.fulfillerStatus == null ? (
-                  <>
-                    <div className={styles.testsOrderedActions}>
-                      <ExtensionSlot state={{ order: row }} name="rejected-ordered-actions-slot" />
-                      <ExtensionSlot state={{ order: row }} name="tests-ordered-actions-slot" />
-                    </div>
-                  </>
-                ) : row.fulfillerStatus === 'IN_PROGRESS' ? (
-                  <>
-                    <div className={styles.testsOrderedActions}>
-                      <ExtensionSlot
-                        className={styles.menuLink}
-                        state={{ order: row }}
-                        name="inprogress-tests-actions-slot"
-                      />
-                    </div>
-                  </>
-                ) : (
-                  <div></div>
-                )}
-              </div>
-            </div>
+              {row.fulfillerStatus === 'DECLINED' && (
+                <OrderDetailRow label={t('reasonForDecline', 'Reason for decline:')} value={row.fulfillerComment} />
+              )}
+            </StructuredListBody>
+          </StructuredListWrapper>
+          {row.fulfillerStatus === 'COMPLETED' && (
+            <Accordion>
+              <AccordionItem
+                title={<span className={styles.accordionTitle}>{t('viewTestResults', 'View test results')}</span>}
+              >
+                <div className={styles.viewResults}>
+                  <ExtensionSlot
+                    className={styles.labResultSlot}
+                    state={{ order: row }}
+                    name="completed-lab-order-results-slot"
+                  />
+                </div>
+              </AccordionItem>
+            </Accordion>
+          )}
+
+          <div className={styles.buttonSection}>
+            {/* @ts-ignore */}
+            {row.fulfillerStatus === 'New' || row.fulfillerStatus === 'RECEIVED' || row.fulfillerStatus == null ? (
+              <>
+                <div className={styles.testsOrderedActions}>
+                  <ExtensionSlot state={{ order: row }} name="rejected-ordered-actions-slot" />
+                  <ExtensionSlot state={{ order: row }} name="tests-ordered-actions-slot" />
+                </div>
+              </>
+            ) : row.fulfillerStatus === 'IN_PROGRESS' ? (
+              <>
+                <div className={styles.testsOrderedActions}>
+                  <ExtensionSlot
+                    className={styles.menuLink}
+                    state={{ order: row }}
+                    name="inprogress-tests-actions-slot"
+                  />
+                </div>
+              </>
+            ) : null}
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 };
