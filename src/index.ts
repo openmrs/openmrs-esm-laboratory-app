7ea875443162020700c3ed42da759eff4393f9d6
Nethmi Rodrigo <nethmi@openmrs.org>
2025-07-23 14:19:07 +0530

    (fix): Update casing in translation string (#323)

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ export const completedLabRequestsTable = getAsyncLifecycle(
   options,
 );
 
-// t('Declined Tests', 'Declined Tests')
+// t('Declined tests', 'Declined tests')
 export const declinedLabRequestsTable = getAsyncLifecycle(
   () => import('./lab-tabs/data-table-extensions/declined-lab-requests-table-extension'),
   options,
