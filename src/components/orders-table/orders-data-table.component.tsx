2e149d1b7e5368ea5ed79355d3da24776e642325
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-12-10 22:32:06 +0530

    Add test results workspace configuration

--- src/components/orders-table/orders-data-table.component.tsx
+++ src/components/orders-table/orders-data-table.component.tsx
@@ -164,7 +164,7 @@ const OrdersDataTable: React.FC<OrdersDataTableProps> = (props) => {
       return groupedOrdersByPatient.filter(
         (orderGroup) =>
           (labTableColumns.includes('name') && orderGroup.patientName.toLowerCase().includes(lowerSearchString)) ||
-          (labTableColumns.includes('patientId') && orderGroup.patientId.toLowerCase().includes(lowerSearchString)) ||
+          (labTableColumns.includes('patientId') && orderGroup.patientId?.toLowerCase().includes(lowerSearchString)) ||
           orderGroup.orders.some((order) => order.orderNumber.toLowerCase().includes(lowerSearchString)),
       );
     }
@@ -220,7 +220,7 @@ const OrdersDataTable: React.FC<OrdersDataTableProps> = (props) => {
     const dispose = showModal('edit-lab-results-modal', {
       closeModal: () => dispose(),
       orders: completedOrders,
-      patient: orders[0]?.patient,
+      patient: completedOrders[0]?.patient,
     });
   };
 
