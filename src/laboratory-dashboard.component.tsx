5c4443550b658e7d4c955a50fb3620d41be8d570
Siddharth Baleja <144931848+siddharthbaleja7@users.noreply.github.com>
2026-01-17 01:18:35 +0530

    (chore) Remove unused overlay component (#646)

--- src/laboratory-dashboard.component.tsx
+++ src/laboratory-dashboard.component.tsx
@@ -5,7 +5,6 @@ import { LaboratoryPictogram, PageHeader, useDefineAppContext } from '@openmrs/e
 import { type DateFilterContext } from './types';
 import LaboratoryOrdersTabs from './lab-tabs/laboratory-tabs.component';
 import LaboratorySummaryTiles from './lab-tiles/laboratory-summary-tiles.component';
-import Overlay from './components/overlay/overlay.component';
 import styles from './laboratory-dashboard.scss';
 
 const LaboratoryDashboard: React.FC = () => {
@@ -23,7 +22,6 @@ const LaboratoryDashboard: React.FC = () => {
       <div>
         <LaboratorySummaryTiles />
         <LaboratoryOrdersTabs />
-        <Overlay />
       </div>
     </div>
   );
