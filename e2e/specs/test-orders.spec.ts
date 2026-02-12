245c2fe6b8eb1566b9e0a2bc04be394b60a4de59
Abhay <abhaynimbalkar03@gmail.com>
2026-02-12 10:27:02 +0530

    Update e2e/specs/test-orders.spec.ts
    Co-authored-by: Nethmi Rodrigo <nethmi@openmrs.org>

--- e2e/specs/test-orders.spec.ts
+++ e2e/specs/test-orders.spec.ts
@@ -49,7 +49,7 @@ test('View test orders', async ({ page }) => {
     await laboratoryPage.expandPatientRow(fullName);
   });
 
-  await test.step('Then I should see the order status, test name, and urgency', async () => {
+  await test.step('Then I should see the order status, test name, and urgency count', async () => {
     await expect(page.getByText(/Status:Order not picked/i)).toBeVisible();
     await expect(page.getByRole('cell', { name: 'serum glucose' })).toBeVisible();
    await expect(page.getByText(/1 Routine/i)).toBeVisible();
